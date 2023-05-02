f9f54571c3fc9aeca38d24ac56b6aa82b1cf6b13
ikverenc <kverenchkhiladzeirakli.personal@gmail.com>
2023-05-02 11:56:10 +0400

    init

--- app.js
+++ app.js
@@ -8,21 +8,21 @@ const { User } = require("./schemas/user.schema.js");
 const { sendVerificationEmail } = require("./email.js");
 
 const allowedOrigins = [
-  /^http:\/\/localhost(:\d+)?$/,
-  /^https?:\/\/.*\.web\.cern\.ch$/,
+    /^http:\/\/localhost(:\d+)?$/,
+    /^https?:\/\/.*\.web\.cern\.ch$/,
 ];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.some((regex) => regex.test(origin))) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET"],
-  allowedHeaders: ["Content-Type"],
-  credentials: true,
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.some((regex) => regex.test(origin))) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
 };
 
 const db = require("./db");
@@ -32,80 +32,74 @@ app.use(express.json());
 
 // Test the database connection and sync the models
 (async () => {
-  try {
-    await db.authenticate();
-    console.log("Database connection has been established successfully.");
-    await db.sync();
-    console.log("tables have been synced.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+    try {
+        await db.authenticate();
+        console.log("Database connection has been established successfully.");
+        await db.sync();
+        console.log("tables have been synced.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
 })();
 
-// HTTP Methods
-// GET - pasuxis migebea
-// POST - shekna
-// PATCH/UPDATE
-// DELETE
-
-//host
-// localhost:8080
-
-// app.post("/puki", (req, res) => {
-//   const { email, password } = req.body;
-//   res.status(201).send({
-//     email,
-//     password,
-//   });
-// });
-
-// localhost:8080/register
 app.post("/register", async (req, res) => {
-  const { email, password } = req.body;
-  const emailVerificationToken = crypto.randomBytes(64).toString("hex");
+    const { email, password } = req.body;
+    const emailVerificationToken = crypto.randomBytes(64).toString("hex");
 
-  try {
-    await User.create({
-      email,
-      password,
-      emailVerificationToken,
-    });
-    sendVerificationEmail(email, emailVerificationToken);
-    res.status(201).json({
-      response: {
-        ok: true,
-      },
-      message: "User created, verification email sent",
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("An error occurred");
-  }
+    try {
+        await User.create({
+            email,
+            password,
+            emailVerificationToken,
+        });
+        sendVerificationEmail(email, emailVerificationToken);
+        res.status(201).json({
+            ok: true,
+            message: "User created, verification email sent",
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            response: {
+                ok: false,
+            },
+            message: "an error occured",
+        });
+    }
 });
 
 app.get("/verify-email", async (req, res) => {
-  const emailVerificationToken = req.query.token;
+    const emailVerificationToken = req.query.token;
 
-  try {
-    const user = await User.findOne({ where: { emailVerificationToken } });
+    try {
+        const user = await User.findOne({ where: { emailVerificationToken } });
 
-    if (!user) {
-      return res.status(400).send("Invalid verification token");
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                message: "Invalid verification token",
+            });
+        }
+
+        await user.update({
+            isEmailVerified: true,
+            emailVerificationToken: null,
+        });
+
+        res.status(200).json({
+            ok: true,
+            message: "Email verified successfully",
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false,
+            message: "Can't verify email",
+        });
     }
-
-    await user.update({
-      isEmailVerified: true,
-      emailVerificationToken: null,
-    });
-
-    res.status(200).send("Email successfully verified");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("An error occurred");
-  }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
--- email.js
+++ email.js
@@ -1,30 +1,30 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "node9818@gmail.com",
-    pass: "icydixwmdxbahdov",
-  },
+    service: "gmail",
+    auth: {
+        user: "node9818@gmail.com",
+        pass: "icydixwmdxbahdov",
+    },
 });
 
 const sendVerificationEmail = (email, emailVerificationToken) => {
-  const mailOptions = {
-    from: "node9818@gmail.com",
-    to: email,
-    subject: "Email Verification",
-    text: `Please click the following link to verify your email: ${process.env.HOST}/verify-email?token=${emailVerificationToken}`,
-  };
+    const mailOptions = {
+        from: "node9818@gmail.com",
+        to: email,
+        subject: "Email Verification",
+        text: `Please click the following link to verify your email: ${process.env.HOST}/verify-email?token=${emailVerificationToken}`,
+    };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      new Error("Failed to send e-mail");
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            new Error("Failed to send e-mail");
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
 };
 
 module.exports = {
-  sendVerificationEmail,
+    sendVerificationEmail,
 };
